0f2170117f84ab26ef3204d2d7c0454db283a3c5
Jessica Kerr <jessitron@atomist.com>
2019-05-13 18:38:54 -0700

    Add path to project name, and make links work

--- lib/routes/projectAnalysisResultUtils.ts
+++ lib/routes/projectAnalysisResultUtils.ts
@@ -1,4 +1,5 @@
 import * as _ from "lodash";
+import * as path from "path";
 import { ProjectAnalysisResult } from "../analysis/ProjectAnalysisResult";
 import { Renderer } from "../tree/TreeBuilder";
 
@@ -7,9 +8,18 @@ export type ProjectAnalysisResultGrouper = (ar: ProjectAnalysisResult) => string
 export const OrgGrouper: ProjectAnalysisResultGrouper = a => _.get(a, "analysis.id.owner");
 
 export const DefaultProjectAnalysisResultRenderer: Renderer<ProjectAnalysisResult> =
-    ar => ({
-        name: ar.analysis.id.repo,
-        size: 1,
-        url: `/projects/${ar.analysis.id.owner}/${ar.analysis.id.repo}`,
-        repoUrl: ar.analysis.id.url,
-    });
+    ar => {
+        const projectName = ar.analysis.id.path ?
+            ar.analysis.id.repo + path.sep + ar.analysis.id.path :
+            ar.analysis.id.repo;
+        const url = ar.analysis.id.path ?
+            ar.analysis.id.url + "/tree/" + (ar.analysis.id.sha || "master") + "/" + ar.analysis.id.path :
+            ar.analysis.id.url;
+
+        return {
+            name: projectName,
+            size: 1,
+            url,
+            repoUrl: ar.analysis.id.url,
+        };
+    };
