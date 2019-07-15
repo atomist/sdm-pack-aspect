c0fa0708e12e7e436df7267b80bdcb11f5ad6efb
Rod Johnson <rod@atomist.com>
2019-07-15 14:34:26 +1000

    Fix test

--- test/element/codeOfConduct.test.ts
+++ test/element/codeOfConduct.test.ts
@@ -26,13 +26,13 @@ describe("codeOfConduct", () => {
 
     it("should find no code of conduct", async () => {
         const p = InMemoryProject.of();
-        const s = await CodeOfConduct.extract(p);
+        const s = await CodeOfConduct.extract(p) as TypedFP<CodeOfConductData>;
         assert(!s);
     });
 
     it("should find test code of conduct", async () => {
         const p = InMemoryProject.of({ path: "CODE_OF_CONDUCT.md", content: testCoC });
-        const s = await CodeOfConduct.extract(p);
+        const s = await CodeOfConduct.extract(p) as TypedFP<CodeOfConductData>;
         assert(!!s);
         assert.strictEqual(s.data.content, testCoC);
         assert.strictEqual(s.data.title, "The Benign Code of Conduct");
@@ -40,7 +40,7 @@ describe("codeOfConduct", () => {
 
     it("should do its best with code of conduct without title", async () => {
         const p = InMemoryProject.of({ path: "CODE_OF_CONDUCT.md", content: "meaningless" });
-        const s = await CodeOfConduct.extract(p);
+        const s = await CodeOfConduct.extract(p) as TypedFP<CodeOfConductData>;
         assert(!!s);
         assert.strictEqual(s.data.content, "meaningless");
         assert(!s.data.title);
