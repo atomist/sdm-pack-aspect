88e0607fc2250978f88406cde8a0c621a41b792e
Jessica Kerr <jessitron@atomist.com>
2019-08-16 11:12:54 -0500

    Define source parameter
    this was hard

--- lib/analysis/offline/spider/analyzeCommand.ts
+++ lib/analysis/offline/spider/analyzeCommand.ts
@@ -1,8 +1,34 @@
-import { CommandListenerInvocation } from "@atomist/sdm";
+import {
+    CommandHandlerRegistration,
+    CommandListenerInvocation,
+    ParametersObject,
+} from "@atomist/sdm";
 
-export const AnalyzeCommandRegistration = {
-    name: "analyzeRepositories", intent: ["analyze"], description: "dig around in repositories and store what we find",
-    listener: async (d: CommandListenerInvocation) => {
-        d.addressChannels("I AM INVOKED");
+export interface AnalyzeCommandParameters {
+    source: "GitHub" | "local";
+}
+
+const AnalyzeCommandParametersDefinition: ParametersObject<AnalyzeCommandParameters> = {
+    source: {
+        description: "find repositories on GitHub or local filesystem",
+        defaultValue: "GitHub",
+        type: {
+            kind: "single",
+            options: [
+                { description: "find repositories on GitHub by owner or query", value: "GitHub" },
+                { description: "find repositories on the local filesystem", value: "local" }],
+        },
+        pattern: /GitHub|local/,
+        validInput: "'GitHub' or 'local'",
+    },
+};
+
+export const AnalyzeCommandRegistration: CommandHandlerRegistration<AnalyzeCommandParameters> = {
+    name: "analyzeRepositories",
+    intent: ["analyze"],
+    description: "dig around in repositories and store what we find",
+    parameters: AnalyzeCommandParametersDefinition,
+    listener: async (d: CommandListenerInvocation<AnalyzeCommandParameters>) => {
+        d.addressChannels("I AM INVOKED with " + d.parameters.source);
     },
 };
