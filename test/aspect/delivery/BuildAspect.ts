2808a4733aa912cb8bdd6e6337696d201bc601ec
Rod Johnson <rod@atomist.com>
2019-08-23 22:48:30 +0000

    Autofix: TypeScript imports
    [atomist:generated] [atomist:autofix=typescript_imports]

--- test/aspect/delivery/BuildAspect.ts
+++ test/aspect/delivery/BuildAspect.ts
@@ -16,11 +16,20 @@
 
 import { logger } from "@atomist/automation-client";
 import { Build } from "@atomist/sdm-pack-build";
-import { Aspect, sha256, } from "@atomist/sdm-pack-fingerprints";
-import { bandFor, Default, } from "../../../lib/util/bands";
+import {
+    Aspect,
+    sha256,
+} from "@atomist/sdm-pack-fingerprints";
+import {
+    bandFor,
+    Default,
+} from "../../../lib/util/bands";
 import { Omit } from "../../../lib/util/omit";
 import { DeliveryAspect } from "./DeliveryAspect";
-import { FindFingerprintsFromGoalExecution, goalExecutionFingerprinter } from "./support/goalListener";
+import {
+    FindFingerprintsFromGoalExecution,
+    goalExecutionFingerprinter,
+} from "./support/goalListener";
 
 export type BuildAspect<DATA = any> = DeliveryAspect<{ build: Build }, DATA>;
 
@@ -87,4 +96,3 @@ export function buildTimeAspect(opts: Omit<Aspect, "name" | "displayName" | "ext
         },
     });
 }
-
