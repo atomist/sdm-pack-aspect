202287711af2369a1e09760447d9f395a531750f
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2019-08-23 17:23:32 -0700

    Actually add it

--- lib/customize/aspects.ts
+++ lib/customize/aspects.ts
@@ -16,28 +16,23 @@
 
 import { LeinDeps } from "@atomist/sdm-pack-clojure/lib/fingerprints/clojure";
 import {
-    DockerfilePath,
-    DockerFrom,
-    DockerPorts,
+  DockerfilePath,
+  DockerFrom,
+  DockerPorts,
 } from "@atomist/sdm-pack-docker";
 import {
-    Aspect,
-    cachingVirtualProjectFinder,
-    fileNamesVirtualProjectFinder,
-    makeVirtualProjectAware,
-    VirtualProjectFinder,
+  Aspect,
+  cachingVirtualProjectFinder,
+  fileNamesVirtualProjectFinder,
+  makeVirtualProjectAware,
+  VirtualProjectFinder,
 } from "@atomist/sdm-pack-fingerprints";
 import { CodeMetricsAspect } from "../aspect/common/codeMetrics";
 import { CodeOwnership } from "../aspect/common/codeOwnership";
 import { CodeOfConduct } from "../aspect/community/codeOfConduct";
-import {
-    License,
-    LicensePresence,
-} from "../aspect/community/license";
-import {
-    ChangelogAspect,
-    ContributingAspect,
-} from "../aspect/community/oss";
+import { License, LicensePresence } from "../aspect/community/license";
+import { NodeEngine } from "../aspect/community/nodeEngine";
+import { ChangelogAspect, ContributingAspect } from "../aspect/community/oss";
 import { globAspect } from "../aspect/compose/globAspect";
 import { branchCount } from "../aspect/git/branchCount";
 import { GitRecency } from "../aspect/git/gitActivity";
@@ -58,51 +53,64 @@ import { TravisScriptsAspect } from "../aspect/travis/travisAspects";
  * if the repository root didn't look like a virtual project.
  */
 export const virtualProjectFinder: VirtualProjectFinder = cachingVirtualProjectFinder(
-    fileNamesVirtualProjectFinder(
-        "package.json", "pom.xml", "build.gradle", "requirements.txt",
-    ));
+  fileNamesVirtualProjectFinder(
+    "package.json",
+    "pom.xml",
+    "build.gradle",
+    "requirements.txt",
+  ),
+);
 
 /**
  * The aspects managed by this SDM.
  * Modify this list to customize with your own aspects.
  */
 export function aspects(): Aspect[] {
-    return [
-        DockerFrom,
-        DockerfilePath,
-        DockerPorts,
-        License,
-        // Based on license, decide the presence of a license: Not spread
-        LicensePresence,
-        SpringBootStarter,
-        TypeScriptVersion,
-        new CodeOwnership(),
-        NpmDependencies,
-        CodeOfConduct,
-        ExposedSecrets,
-        TravisScriptsAspect,
-        branchCount,
-        GitRecency,
-        // This is expensive as it requires deeper cloning
-        // gitActiveCommitters(30),
-        // This is also expensive
-        CodeMetricsAspect,
-        // StackAspect,
-        // CiAspect,
-        // JavaBuild,
-        // Don't show these
-        globAspect({ name: "csproject", displayName: undefined, glob: "*.csproj" }),
-        globAspect({ name: "snyk", displayName: undefined, glob: ".snyk" }),
-        ChangelogAspect,
-        ContributingAspect,
-        globAspect({ name: "azure-pipelines", displayName: "Azure pipeline", glob: "azure-pipelines.yml" }),
-        globAspect({ name: "readme", displayName: "Readme file", glob: "README.md" }),
-        CsProjectTargetFrameworks,
-        SpringBootVersion,
-        // allMavenDependenciesAspect,    // This is expensive
-        DirectMavenDependencies,
-        PythonDependencies,
-        K8sSpecs,
-        LeinDeps,
-    ].map(aspect => makeVirtualProjectAware(aspect, virtualProjectFinder));
+  return [
+    DockerFrom,
+    DockerfilePath,
+    DockerPorts,
+    License,
+    // Based on license, decide the presence of a license: Not spread
+    LicensePresence,
+    SpringBootStarter,
+    TypeScriptVersion,
+    new CodeOwnership(),
+    NodeEngine,
+    NpmDependencies,
+    CodeOfConduct,
+    ExposedSecrets,
+    TravisScriptsAspect,
+    branchCount,
+    GitRecency,
+    // This is expensive as it requires deeper cloning
+    // gitActiveCommitters(30),
+    // This is also expensive
+    CodeMetricsAspect,
+    // StackAspect,
+    // CiAspect,
+    // JavaBuild,
+    // Don't show these
+    globAspect({ name: "csproject", displayName: undefined, glob: "*.csproj" }),
+    globAspect({ name: "snyk", displayName: undefined, glob: ".snyk" }),
+    ChangelogAspect,
+    ContributingAspect,
+    globAspect({
+      name: "azure-pipelines",
+      displayName: "Azure pipeline",
+      glob: "azure-pipelines.yml",
+    }),
+    globAspect({
+      name: "readme",
+      displayName: "Readme file",
+      glob: "README.md",
+    }),
+    CsProjectTargetFrameworks,
+    SpringBootVersion,
+    // allMavenDependenciesAspect,    // This is expensive
+    DirectMavenDependencies,
+    PythonDependencies,
+    K8sSpecs,
+    LeinDeps,
+  ].map(aspect => makeVirtualProjectAware(aspect, virtualProjectFinder));
 }
