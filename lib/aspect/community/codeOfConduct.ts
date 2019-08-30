f6675d9ebc4d273133b7f27d8431a9b476612ced
Rod Johnson <rod@atomist.com>
2019-08-30 21:51:01 +0000

    Autofix: tslint
    [atomist:generated] [atomist:autofix=tslint]

--- lib/aspect/community/codeOfConduct.ts
+++ lib/aspect/community/codeOfConduct.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { Aspect, fingerprintOf, } from "@atomist/sdm-pack-fingerprints";
+import { Aspect, fingerprintOf } from "@atomist/sdm-pack-fingerprints";
 import { ContentClassifier } from "./ContentClassifier";
 
 export const CodeOfConductType = "code-of-conduct";
