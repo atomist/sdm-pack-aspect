638aba0b5561fcb6b0bb740b313c03d42821eb48
Jessica Kerr <jessitron@atomist.com>
2019-08-30 18:07:42 -0700

    Make graph of times

--- lib/analysis/tracking/analysisTrackingRoutes.ts
+++ lib/analysis/tracking/analysisTrackingRoutes.ts
@@ -13,9 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { logger } from "@atomist/automation-client";
+import * as _ from "lodash";
 import { AnalysisTrackingPage } from "../../../views/analysisTrackingPage";
+import { AspectTrackingPage } from "../../../views/aspectTrackingPage";
 import { renderStaticReactNode } from "../../../views/topLevelPage";
 import { WebAppConfig } from "../../routes/web-app/webAppConfig";
 
@@ -26,6 +27,37 @@ export function exposeAnalysisTrackingPage(conf: WebAppConfig): void {
 
             res.send(renderStaticReactNode(
                 AnalysisTrackingPage(data), `Analyzing projects`,
+                conf.instanceMetadata));
+        } catch (e) {
+            logger.error(e.stack);
+            res.status(500).send("failure");
+        }
+    });
+
+    conf.express.get("/analysis/aspects", ...conf.handlers, async (req, res) => {
+        try {
+            const data = conf.analysisTracking.report();
+
+            const allAspectTimings = _.flatten(data.analyses
+                .map(a => _.flatten(a.repos
+                    .filter(r => r.progress === "Stopped")
+                    .map(r => r.aspects))));
+            const timingsByAspect = _.groupBy(allAspectTimings, a => a.aspectName);
+
+            const summarized = Object.values(timingsByAspect).map(timings => {
+                return {
+                    aspectName: timings[0].aspectName,
+                    runs: timings.length,
+                    totalFingerprints: timings.map(t => t.fingerprintsFound).reduce((a, b) => a + b, 0),
+                    failures: timings.filter(f => !!f.error).length,
+                    minMillis: Math.min(...timings.map(t => t.millisTaken)),
+                    maxMillis: Math.max(...timings.map(t => t.millisTaken)),
+                    totalTimeTaken: timings.map(t => t.millisTaken).reduce((a, b) => a + b, 0),
+                };
+            });
+
+            res.send(renderStaticReactNode(
+                AspectTrackingPage({ aspectPerformances: summarized }), `Performance of Aspects during Analysis`,
                 conf.instanceMetadata));
         } catch (e) {
             logger.error(e.stack);
---
+++ views/aspectTrackingPage.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+interface AspectPerformance {
+    aspectName: string;
+    runs: number;
+    totalFingerprints: number;
+    minMillis: number;
+    maxMillis: number;
+    failures: number;
+    totalTimeTaken: number;
+}
+interface AspectTrackingProps {
+    aspectPerformances: AspectPerformance[];
+}
+
+function displayAspectPerformance(world: { widthOfGraph: number, pixelsPerMilli: number }, ap: AspectPerformance): React.ReactElement {
+
+    let belowLowWidth = Math.round(ap.minMillis * world.pixelsPerMilli);
+    let betweenWidth = Math.round((ap.maxMillis - ap.minMillis) * world.pixelsPerMilli);
+    if (betweenWidth < 5) {
+        betweenWidth = 6;
+        belowLowWidth = Math.max(0, belowLowWidth - 3);
+    }
+    if ((belowLowWidth + betweenWidth) > world.widthOfGraph) {
+        belowLowWidth = world.widthOfGraph - betweenWidth;
+    }
+
+    return <tr>
+        <td>{ap.aspectName}</td><td>{ap.runs}</td><td>{ap.totalFingerprints}</td>
+        <td>{ap.failures}</td><td className="runTiming">{ap.minMillis}ms</td>
+        <td><div className="envisionSpeed" style={{ width: world.widthOfGraph + "px" }}>
+            <div className="belowLow" style={{ width: belowLowWidth + "px" }}>{ap.minMillis}</div>
+            <div className="betweenLowAndHigh" style={{ width: betweenWidth + "px" }}
+                title={ap.minMillis + "-" + ap.maxMillis + "ms"}>- {ap.maxMillis}</div>
+
+        </div></td>
+        <td className="runTiming">{ap.maxMillis}ms</td>
+    </tr >;
+}
+
+function sortAspectPerformance(a1: AspectPerformance, a2: AspectPerformance): number {
+    return a2.totalTimeTaken - a1.totalTimeTaken;
+}
+
+export function AspectTrackingPage(props: AspectTrackingProps): React.ReactElement {
+    if (props.aspectPerformances.length === 0) {
+        return <div>No analyses in progress.
+            Start one at the command line:{" "}
+            <span className="typeThisAtCommandLine">atomist analyze local repositories</span></div>;
+    }
+
+    const widthOfGraph = 500;
+    const minniestMin = Math.min(...props.aspectPerformances.map(a => a.minMillis));
+    const maxiestMax = Math.max(...props.aspectPerformances.map(a => a.maxMillis));
+    const pixelsPerMilli = widthOfGraph / (maxiestMax - minniestMin);
+    console.log(`From ${minniestMin} to ${maxiestMax}, that leaves ${pixelsPerMilli} px/ms`);
+    return <table>
+        <thead><tr>
+            <th>Name</th><th>Runs</th><th>FPs</th><th>Fails</th><th className="runHeader">Fastest run</th>
+            <th><div style={{ width: widthOfGraph + "px" }}>
+                <span className="minniestMin">{minniestMin} ms</span> --- Range of time taken ---
+                <span className="maxiestMax">{maxiestMax} ms</span></div></th>
+            <th className="runHeader">Slowest run</th>
+        </tr></thead>
+        {props.aspectPerformances.sort(sortAspectPerformance).map(ap => displayAspectPerformance({ widthOfGraph, pixelsPerMilli }, ap))}
+    </table>;
+}
