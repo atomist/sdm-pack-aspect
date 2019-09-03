b11ab81d75be92bc33537f16d168bd669f87f3d3
Christian Dupuis <cd@atomist.com>
2019-09-03 08:51:58 +0200

    Polish

--- lib/aspect/AspectReportDetailsRegistry.ts
+++ lib/aspect/AspectReportDetailsRegistry.ts
@@ -16,19 +16,34 @@
 
 import { Aspect } from "@atomist/sdm-pack-fingerprint";
 
+/**
+ * Details needed for the web-app to show the aspects
+ */
 export interface AspectReportDetails {
+    /** Short name of the aspect as used in headlines etc */
     shortName?: string;
+    /** Longish description of the aspect */
     description?: string;
+    /** What is the aspect measuring: a version, a docker image tag */
     unit?: string;
+    /** The url to the sunburst data to use when rendering the chart on the web-app */
     url?: string;
+    /** Category to place the aspect in */
     category?: string;
+    /** Does this aspect support setting targets etc */
     manage?: boolean;
 }
 
+/**
+ * Extension to core Aspect to have additional metadata
+ */
 export interface AspectWithReportDetails<F = any> extends Aspect<F> {
     details?: AspectReportDetails;
 }
 
+/**
+ * Add report details to the provided Aspect
+ */
 export function enrich(aspect: Aspect, details: AspectReportDetails): AspectWithReportDetails {
     return {
         ...aspect,
