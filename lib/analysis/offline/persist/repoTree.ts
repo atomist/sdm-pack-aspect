24a33fb018f486b83b043730f22934bcb1f6f8cf
Christian Dupuis <cd@atomist.com>
2019-09-05 10:55:39 +0200

    Return providerId for web-app

--- lib/analysis/offline/persist/repoTree.ts
+++ lib/analysis/offline/persist/repoTree.ts
@@ -55,7 +55,7 @@ SELECT row_to_json(fingerprint_groups) FROM (
          (
              SELECT json_agg(row_to_json(repo)) FROM (
                   SELECT
-                    repo_snapshots.id, repo_snapshots.owner, repo_snapshots.name, repo_snapshots.url, 1 as size, repo_fingerprints.path
+                    repo_snapshots.id, repo_snapshots.owner, repo_snapshots.name, repo_snapshots.url, repo_snapshots.provider_id as providerId, 1 as size, repo_fingerprints.path
                   FROM repo_fingerprints, repo_snapshots
                    WHERE repo_fingerprints.fingerprint_id = fingerprints.id
                     AND repo_snapshots.id = repo_fingerprints.repo_snapshot_id
