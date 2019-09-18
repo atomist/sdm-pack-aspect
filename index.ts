ba70f0d0fee5d9008f4425ae480f3b15b5eef2b9
Rod Johnson <rod@atomist.com>
2019-09-18 11:10:01 -0700

    Improve reviewer aspect

--- index.ts
+++ index.ts
@@ -38,6 +38,7 @@ export * from "./lib/aspect/git/gitIgnore";
 export * from "./lib/aspect/git/dateUtils";
 export * from "./lib/aspect/secret/exposedSecrets";
 export * from "./lib/aspect/score/ScoredAspect";
+export * from "./lib/aspect/common/reviewerAspect";
 
 import * as commonScorers from "./lib/scorer/commonScorers";
 import * as commonTaggers from "./lib/tagger/commonTaggers";
--- lib/aspect/common/reviewerAspect.ts
+++ lib/aspect/common/reviewerAspect.ts
@@ -21,40 +21,90 @@ import {
 } from "@atomist/automation-client";
 import { ReviewerRegistration } from "@atomist/sdm";
 import {
-    Aspect,
-    sha256,
+    Aspect, fingerprintOf, FP,
 } from "@atomist/sdm-pack-fingerprint";
 import { CodeInspection } from "@atomist/sdm/lib/api/registration/CodeInspectionRegistration";
-import { AspectMetadata } from "../compose/commonTypes";
+import { AspectMetadata, CountAspect } from "../compose/commonTypes";
+import { ClassificationAspect, projectClassificationAspect } from "../compose/classificationAspect";
 
 export type EligibleReviewer = ReviewerRegistration | CodeInspection<ProjectReview, NoParameters>;
 
 /**
- * Create fingerprints from the output of this reviewer
+ * Emit fingerprint aspect, count aspect and classification aspect for the given review comment
  */
-export function reviewerAspect(opts: AspectMetadata & {
+export function reviewerAspects(opts: AspectMetadata & {
+    reviewer: EligibleReviewer,
+}): Aspect[] {
+    return [
+        reviewCommentAspect(opts),
+        reviewCommentCountAspect(opts),
+        reviewCommentClassificationAspect(opts),
+    ];
+}
+
+export function isReviewCommentFingerprint(fp: FP): fp is FP<ReviewComment> {
+    const maybe = fp.data as ReviewComment;
+    return !!maybe && !!maybe.subcategory && !!maybe.detail;
+}
+
+/**
+ * Create fingerprints from the output of this reviewer.
+ * Every fingerprint is unique
+ */
+function reviewCommentAspect(opts: AspectMetadata & {
     reviewer: EligibleReviewer,
 }): Aspect<ReviewComment> {
     const inspection = isReviewerRegistration(opts.reviewer) ? opts.reviewer.inspection : opts.reviewer;
     return {
         ...opts,
+        name: reviewCommentAspectName(opts.name),
         extract: async (p, pli) => {
             const result = await inspection(p, { ...pli, push: pli });
             if (!result) {
                 return [];
             }
             return result.comments.map(data => {
-                const shaAble = {
-                    detail: data.detail,
-                    category: data.category,
-                    subcategory: data.subcategory,
-                };
-                return {
+                return fingerprintOf({
                     type: opts.name,
-                    name: opts.name,
                     data,
-                    sha: sha256(JSON.stringify(shaAble)),
-                };
+                });
+            });
+        },
+    };
+}
+
+function reviewCommentAspectName(name: string): string {
+    return "instance_" + name;
+}
+
+function reviewCommentClassificationAspect(opts: AspectMetadata & {
+    reviewer: EligibleReviewer,
+}): ClassificationAspect {
+    const requiredType = reviewCommentAspectName(opts.name);
+    return projectClassificationAspect({
+            name: `has_${opts.name}`,
+            displayName: opts.displayName,
+        },
+        {
+            tags: `has-${opts.name}`,
+            reason: `Has review comment ${opts.name}`,
+            testFingerprints: async fps => fps.some(fp => isReviewCommentFingerprint(fp) && fp.type === requiredType),
+        });
+}
+
+function reviewCommentCountAspect(opts: AspectMetadata & {
+    reviewer: EligibleReviewer,
+}): CountAspect {
+    const requiredType = reviewCommentAspectName(opts.name);
+    return {
+        name: `count_${opts.name}`,
+        displayName: opts.displayName,
+        extract: async () => [],
+        consolidate: async fps => {
+            const count = fps.filter(fp => isReviewCommentFingerprint(fp) && fp.type === requiredType).length;
+            return fingerprintOf({
+                type: "x",
+                data: { count },
             });
         },
     };
