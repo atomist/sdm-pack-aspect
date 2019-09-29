7401fa1a97d2e92bd2ab34b91a4411c8d3613884
Rod Johnson <rod@atomist.com>
2019-09-29 02:00:50 +0000

    Autofix: TypeScript imports
    [atomist:generated] [atomist:autofix=typescript_imports]

--- test/aspect/common/reviewerAspects.test.ts
+++ test/aspect/common/reviewerAspects.test.ts
@@ -16,12 +16,17 @@
 
 import {
     InMemoryProject,
-    NoParameters, Project,
-    ProjectReview, ReviewComment,
+    NoParameters,
+    Project,
+    ProjectReview,
+    ReviewComment,
 } from "@atomist/automation-client";
 import { CodeTransform } from "@atomist/sdm";
 import { toArray } from "@atomist/sdm-core/lib/util/misc/array";
-import { Aspect, fingerprintOf } from "@atomist/sdm-pack-fingerprint";
+import {
+    Aspect,
+    fingerprintOf,
+} from "@atomist/sdm-pack-fingerprint";
 import { CodeInspection } from "@atomist/sdm/lib/api/registration/CodeInspectionRegistration";
 import * as assert from "assert";
 import {
