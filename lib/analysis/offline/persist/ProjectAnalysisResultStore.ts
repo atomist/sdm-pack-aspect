89b0f463f53e7f4cb9d38ffb3f77ab632ebb1da5
Jessica Kerr <jessitron@atomist.com>
2019-10-17 03:31:21 +0000

    Autofix: TypeScript header
    [atomist:generated] [atomist:autofix=typescript_header]
    
    Autofix: tslint
    
    [atomist:generated] [atomist:autofix=tslint]
    
    Autofix: TypeScript imports
    
    [atomist:generated] [atomist:autofix=typescript_imports]

--- lib/analysis/offline/persist/ProjectAnalysisResultStore.ts
+++ lib/analysis/offline/persist/ProjectAnalysisResultStore.ts
@@ -16,7 +16,10 @@
 
 import { RepoRef } from "@atomist/automation-client";
 import { FP } from "@atomist/sdm-pack-fingerprint";
-import { Analyzed, AnalyzedWorkspace } from "../../../aspect/AspectRegistry";
+import {
+    Analyzed,
+    AnalyzedWorkspace,
+} from "../../../aspect/AspectRegistry";
 import {
     PlantedTree,
     TagUsage,
@@ -106,8 +109,8 @@ export interface ProjectAnalysisResultStore {
      * @return {Promise<PlantedTree>}
      */
     aspectDriftTree(workspaceId: string,
-        percentile: number,
-        options?: { repos?: boolean, type?: string }): Promise<PlantedTree>;
+                    percentile: number,
+                    options?: { repos?: boolean, type?: string }): Promise<PlantedTree>;
 
     /**
      * How many repos we've analyzed
@@ -174,9 +177,9 @@ export interface ProjectAnalysisResultStore {
      * @param name fingerprint name (optional)
      */
     fingerprintsInWorkspace(workspaceId: string,
-        distinct: boolean,
-        type?: string,
-        name?: string): Promise<Array<FP & { id: string }>>;
+                            distinct: boolean,
+                            type?: string,
+                            name?: string): Promise<Array<FP & { id: string }>>;
 
     fingerprintsForProject(id: string): Promise<Array<FP & { timestamp: Date, commitSha: string }>>;
 
