487cefa6e6ff6c987b694387d5184c3c341a7ac4
Jessica Kerr <jessitron@atomist.com>
2019-10-22 10:11:59 -0500

    Remove tags method and endpoint
    BREAKING CHANGE: removed ProjectAnalysisResultStore.tags
    I think this is an old way of retrieving tags

--- lib/analysis/offline/persist/PostgresProjectAnalysisResultStore.ts
+++ lib/analysis/offline/persist/PostgresProjectAnalysisResultStore.ts
@@ -74,8 +74,8 @@ export class PostgresProjectAnalysisResultStore implements ProjectAnalysisResult
     }
 
     public aspectDriftTree(workspaceId: string,
-                           percentile: number,
-                           options?: { repos?: boolean, type?: string }): Promise<PlantedTree> {
+        percentile: number,
+        options?: { repos?: boolean, type?: string }): Promise<PlantedTree> {
         return !!options && !!options.type ?
             driftTreeForSingleAspect(workspaceId, percentile, options, this.clientFactory) :
             driftTreeForAllAspects(workspaceId, percentile, this.clientFactory);
@@ -127,9 +127,9 @@ WHERE workspace_id ${workspaceId === "*" ? "<>" : "="} $1
      * @return {Promise<ProjectAnalysisResult[]>}
      */
     private async loadInWorkspaceInternal(workspaceId: string,
-                                          deep: boolean,
-                                          additionalWhereClause: string = "true",
-                                          additionalParameters: any[] = []): Promise<ProjectAnalysisResult[]> {
+        deep: boolean,
+        additionalWhereClause: string = "true",
+        additionalParameters: any[] = []): Promise<ProjectAnalysisResult[]> {
         const workspaceEquals = workspaceId !== "*" ? "=" : "<>";
         const reposOnly = `SELECT id, owner, name, url, commit_sha, timestamp, repo_snapshots.workspace_id
 FROM repo_snapshots
@@ -255,19 +255,6 @@ GROUP BY repo_snapshots.workspace_id, repo_snapshots.id`;
         }, []);
     }
 
-    public tags(workspaceId: string): Promise<TagUsage[]> {
-        const sql = `SELECT fp.name as name, fp.data ->> 'description' as description, fp.feature_name as parent, count(fp.name)
-  FROM repo_snapshots rs, repo_fingerprints j, fingerprints fp
-  WHERE j.repo_snapshot_id = rs.id and j.fingerprint_id = fp.id
-    AND rs.workspace_id ${workspaceId === "*" ? "<>" : "="} $1
-    AND fp.data ->> 'reason' IS NOT NULL
-  GROUP BY fp.name, parent, description`;
-        return doWithClient(sql, this.clientFactory, async client => {
-            const result = await client.query(sql, [workspaceId]);
-            return result.rows;
-        }, []);
-    }
-
     public fingerprintUsageForType(workspaceId: string, type?: string): Promise<FingerprintUsage[]> {
         return fingerprintUsageForType(this.clientFactory, workspaceId, type);
     }
@@ -371,7 +358,7 @@ GROUP BY repo_snapshots.workspace_id, repo_snapshots.id`;
         }
     }
     private async persistRepoSnapshot(client: ClientBase, snapshotId: string, workspaceId: string, repoRef: RepoRef,
-                                      query?: string) {
+        query?: string) {
         const shaToUse = repoRef.sha;
         const repoSnapshotsInsertSql = `INSERT INTO repo_snapshots (id, workspace_id, provider_id, owner, name, url,
             commit_sha, query, timestamp)
@@ -479,10 +466,10 @@ type StoredFingerprint = FP & { id: string };
  * @return {Promise<FP[]>}
  */
 async function fingerprintsInWorkspace(clientFactory: ClientFactory,
-                                       workspaceId: string,
-                                       distinct: boolean,
-                                       type?: string,
-                                       name?: string): Promise<StoredFingerprint[]> {
+    workspaceId: string,
+    distinct: boolean,
+    type?: string,
+    name?: string): Promise<StoredFingerprint[]> {
     const workspaceEquals = workspaceId === "*" ? "<>" : "=";
     const sql = `SELECT ${distinct ? "DISTINCT" : ""} f.name, f.id, f.feature_name as type, f.sha, f.data,
      f.display_name as "displayName", f.display_value as "displayValue", rf.path
@@ -515,8 +502,8 @@ WHERE rs.workspace_id ${workspaceEquals} $1
 }
 
 async function fingerprintsForProject(clientFactory: ClientFactory,
-                                      workspaceId: string,
-                                      snapshotId: string): Promise<Array<FP & { timestamp: Date, commitSha: string }>> {
+    workspaceId: string,
+    snapshotId: string): Promise<Array<FP & { timestamp: Date, commitSha: string }>> {
     const sql = `SELECT f.name as fingerprintName, f.feature_name, f.sha, f.data, rf.path, rs.timestamp, rs.commit_sha,
     f.display_name as "displayName", f.display_value as "displayValue"
 FROM repo_fingerprints rf, repo_snapshots rs, fingerprints f
