69281cb8d3f007167003c7474d63443860f2ea48
Jessica Kerr <jessitron@atomist.com>
2019-10-22 15:06:02 +0000

    Autofix: tslint
    [atomist:generated] [atomist:autofix=tslint]

--- lib/aspect/DefaultAspectRegistry.ts
+++ lib/aspect/DefaultAspectRegistry.ts
@@ -74,8 +74,8 @@ export class DefaultAspectRegistry implements AspectRegistry, AspectReportDetail
     }
 
     public async tagAndScoreRepos(workspaceId: string,
-        repos: ProjectAnalysisResult[],
-        tsOpts: TagAndScoreOptions): Promise<ScoredRepo[]> {
+                                  repos: ProjectAnalysisResult[],
+                                  tsOpts: TagAndScoreOptions): Promise<ScoredRepo[]> {
         const tagged = await showTiming(
             `Tag ${repos.length} repos with ${this.taggers.length} taggers`,
             async () => this.tagRepos({
@@ -134,7 +134,7 @@ export class DefaultAspectRegistry implements AspectRegistry, AspectReportDetail
     }
 
     private async tagRepos(tagContext: TagContext,
-        repos: ProjectAnalysisResult[]): Promise<TaggedRepo[]> {
+                           repos: ProjectAnalysisResult[]): Promise<TaggedRepo[]> {
         const simpleTaggers = this.taggers.filter(isTagger);
         const workspaceSpecificTaggers = await Promise.all(this.taggers
             .filter(td => !isTagger(td))
